c2e9f2e0004c63dd4596189881c49915dfad66e6
Geist5000 <geist5000@gmail.com>
2020-12-07 23:22:47 +0100

    day 2

---
+++ 2/second.py
@@ -0,0 +1,31 @@
+
+class Policy:
+    def __init__(self,min,max,character):
+        self.min = min
+        self.max = max
+        self.character = character
+    def isValid(self,password):
+        return sum((password[self.min]==self.character,password[self.max]==self.character)) == 1
+    def parse(data):
+        polData = data.split(" ")
+        character = polData[1]
+        minMax = polData[0].split("-")
+        return Policy(int(minMax[0]),int(minMax[1]),character)
+
+
+def isValid(line):
+    splitted = line.split(":")
+    pol = Policy.parse(splitted[0])
+    return pol.isValid(splitted[1])
+
+if __name__ == "__main__":
+    lines = []
+    with open("data.txt","r") as f:
+        lines = list(map(lambda x: x.replace("\n",""),f.readlines()))
+        
+    count = 0
+    for l in lines:
+        if(isValid(l)):
+            count += 1
+    print(count)
+    
