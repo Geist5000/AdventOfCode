1f20e16ca1e3db185a01397ff98671473aca9a0e
Geist5000 <geist5000@gmail.com>
2020-12-14 09:02:53 +0100

    daily challenger day 12 second part

---
+++ 2020/12/second.py
@@ -0,0 +1,55 @@
+import re
+def changeDirection(facingDir,angle):
+	# Does work for all mutliples of 90 degrees
+	# does return a copy of the facingDir
+	# east = (1,0); south = 0,-1; west = (-1,0) nord = (0,1);
+	rounds = abs(int(angle/90))
+	x = facingDir[0]
+	y = facingDir[1]
+	print("--" + str(rounds))
+	for i in range(rounds):
+		print(angle,x,y)
+		if(angle<0):
+			y *= -1
+			
+		else:
+			x *= -1
+		copy = x
+		x = y
+		y = copy
+	return (x,y)
+			
+	
+
+regex = r'(N|S|E|W|L|R|F)([0-9]+)'
+lines = []
+
+coord = (0,0)
+
+facing = (10,1)
+
+with open("data.txt") as f:
+    lines = list(map(lambda l: re.fullmatch(regex,l.replace("\n","")),f.readlines()))
+	
+for c in lines:
+	action = c.group(1)
+	number = int(c.group(2))
+	if action == "L":
+		facing = changeDirection(facing,-number)
+	elif action == "R":
+		facing = changeDirection(facing,number)
+	elif action == "F":
+		coord = (coord[0] + facing[0]*number,coord[1] + facing[1]*number)
+	elif action == "N":
+		facing = (facing[0],facing[1] + number)
+	elif action == "S":
+		facing = (facing[0], facing[1] - number)
+	elif action == "E":
+		facing = (facing[0] + number,facing[1])
+	elif action == "W":
+		facing = (facing[0] - number,facing[1])
+	else:
+		raise Exception("not a valid action")
+print(coord)
+print(abs(coord[0])+abs(coord[1]))
+		
