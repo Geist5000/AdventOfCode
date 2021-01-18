65c76653b5c294cf23d11ec5a8b13e904f1698cc
Geist5000 <geist5000@gmail.com>
2021-01-18 18:27:07 +0100

    started part two of day 13
    (currently runtime ist too long)

--- 2020/13/second.py
+++ 2020/13/second.py
@@ -1,37 +1,49 @@
+def solve(busIds):
+    # list of tuples with (index,id)
+    if(len(busIds) > 2):
+        prevNumber = solve(busIds[:len(busIds)-1])
+        lastNumber = busIds[-1]
+        prevIndex = prevNumber[0]
+        lastIndex = lastNumber[0]
+
+        lastNumber = (lastIndex - prevIndex,lastNumber[1])
+        prevNumber = (0,prevNumber[1])
+        busIds = [prevNumber,lastNumber]
+    
+    m = max(map(lambda x: int(x[1]),busIds))
+    currentIndex = 0
+    while(True):
+        start = None
+        for i,id in busIds:
+            if(start == None):
+                
+                current = int(currentIndex*m/id)*id
+                start = current
+            else:
+                diff = id-start%id
+                if(diff != i):
+                    f = False
+                    break
+                else:
+                    return (i,start+diff)
+        currentIndex += 1
+        
+
+
+
+
+
+
 lines = []
 
 
-with open("data.txt") as f:
+with open("testInput.txt") as f:
     lines = list(map(lambda l: l.replace("\n",""),f.readlines()))
 
 
 for l in lines[1:]:
 
     busIds = l.split(",")
+    busIds = list(map(lambda x: (x[0],int(x[1])),filter(lambda x: x[1] != "x", enumerate(busIds))))
 
-    m = max(map(int,filter(lambda x: x != "x",busIds)))
-
-    found = False
-    currentIndex = 0
-    while(not found):
-        start = None
-        f = False
-        for i,id in enumerate(busIds):
-            if id != "x":
-                time = int(id)
-                if(start == None):
-                    
-                    current = int(currentIndex*m/time)*time
-                    start = current
-                else:
-                    diff = time-start%time
-                    if(diff != i):
-                        f = False
-                        break
-                    else:
-                        f = True
-        if(f):
-            found = True
-            print(start)
-            break
-        currentIndex += 1
+    print(solve(busIds))
