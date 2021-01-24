4b50cd17bf491dd9db7308db1232da32f15808aa
Geist5000 <geist5000@gmail.com>
2021-01-24 17:30:13 +0100

    solution day 13 part two

--- 2020/13/README.md
+++ 2020/13/README.md
@@ -1,3 +1,107 @@
 # AdventOfCode
 
 To get the challenge click [here](https://adventofcode.com/2020/day/13)
+
+# Notes
+
+input: 4x6
+
+finde ersten Index für 4 index 0 und 6 index 2
+
+match: 6
+
+nächstes match ist (match wenn 6*x-2 = 4*y):
+
+6%4=2
+
+1*6 = 6     (match 1*4 = 4)
+
+2*6 = 12    (kein match)
+
+3*6 = 18    (match) (4*4 = 16)
+
+4*6 = 24    (kein match)
+
+5*6 = 30    (match 6*4 = 28)
+
+6*6 = 36    (kein match)
+
+7*6 = 42    (match 10*4 = 40)
+
+8*6 = 48    (kein match)
+
+9*6 = 54    (match 13*4 = 52)
+
+10 * 6 = 60 (kein match)
+
+11 * 6 = 66 (match 16*4 = 64)
+
+diff der matches = 12
+
+gleicher ablauf mit 4x15:
+
+Matches (15*x-2 = 4*y):
+
+15%4=3
+
+1*15 = 15   (kein match)
+
+2*15 = 30   (match 7*4 = 28)
+
+3*15 = 45   (kein match)
+
+4*15 = 60   (kein match)
+
+5*15 = 75   (kein match)
+
+6*15 = 90   (match 22*4 = 88)
+
+7*15 = 105  (kein match)
+
+8*15 = 120  (kein match)
+
+9*15 = 135  (kein match)
+
+10*15 = 150 (match 37*4 = 148)
+
+11*15 = 165 (kein match)
+
+12*15 = 180 (kein match)
+
+13*15 = 195 (kein match)
+
+14*15 = 210 (match 52*4=208)
+
+diff der matches = 60
+
+gleicher ablauf mit 11x15:
+
+Matches (15*x-2 = 11*y || (15*x-2)%11 = 0):
+
+15%11=4
+
+
+1*15=15         (kein match)
+2*15=30         (kein match)
+3*15=45         (kein match)
+4*15=60         (kein match)
+5*15=75         (kein match)
+6*15=90         (match 8*11=88)
+7*15=105        (kein match)
+8*15=120        (kein match)
+9*15=135        (kein match)
+10*15=150       (kein match)
+11*15=165       (kein match)
+12*15=180       (kein match)
+13*15=195       (kein match)
+14*15=210       (kein match)
+15*15=225       (kein match)
+16*15=240       (kein match)
+17*15=255       (match 23*11=253)
+18*15=270       (kein match)
+19*15=285       (kein match)
+20*15=300       (kein match)
+
+diff der matches = 165
+
+Frage: wie kann ich das match vervielfachen um auf die nächste valide zahl zu kommen
--- 2020/13/second.py
+++ 2020/13/second.py
@@ -1,32 +1,42 @@
 def solve(busIds):
-    # list of tuples with (index,id)
-    if(len(busIds) > 2):
-        prevNumber = solve(busIds[:len(busIds)-1])
-        lastNumber = busIds[-1]
-        prevIndex = prevNumber[0]
-        lastIndex = lastNumber[0]
+    # list of tuples with (index,id)#
 
-        lastNumber = (lastIndex - prevIndex,lastNumber[1])
-        prevNumber = (0,prevNumber[1])
-        busIds = [prevNumber,lastNumber]
+    if(len(busIds) == 2):
+        return findMatches(busIds)
+    else:
+        prevMatches = solve(busIds[:-1])
+        diff = prevMatches[1]-prevMatches[0]
+
+        matches = []
+        index =1
+        while(len(matches)<2):
+            res =prevMatches[0]+index*diff
+            match = checkIfMatch(busIds,res)
+            if(match):
+                matches.append(res)
+            index += 1
+        return matches
     
-    m = max(map(lambda x: int(x[1]),busIds))
-    currentIndex = 0
-    while(True):
-        start = None
-        for i,id in busIds:
-            if(start == None):
-                
-                current = int(currentIndex*m/id)*id
-                start = current
-            else:
-                diff = id-start%id
-                if(diff != i):
-                    f = False
-                    break
-                else:
-                    return (i,start+diff)
-        currentIndex += 1
+
+    
+
+
+def findMatches(numbers,count = 2):
+    # list with two values which are (index,id)
+    bigger = max(numbers,key=lambda x: x[1])
+    matches = []
+    index = 1
+    while(len(matches) < count):
+        res = index * bigger[1]-bigger[0]
+        match = checkIfMatch(numbers,res)
+        if(match):
+            matches.append(res)
+        index += 1
+    return matches
+
+def checkIfMatch(constraints,value):
+    return all(map(lambda x: (value+x[0])%x[1] == 0,constraints))
+
         
 
 
@@ -37,13 +47,16 @@ def solve(busIds):
 lines = []
 
 
-with open("testInput.txt") as f:
+with open("data.txt") as f:
     lines = list(map(lambda l: l.replace("\n",""),f.readlines()))
 
 
 for l in lines[1:]:
 
     busIds = l.split(",")
+
+    # converts the input string (1,x,5,8,x,9) into a list of tuples
+    #  which have the index of the number as first value and the number itself as second value
     busIds = list(map(lambda x: (x[0],int(x[1])),filter(lambda x: x[1] != "x", enumerate(busIds))))
 
-    print(solve(busIds))
+    print(solve(busIds)[0])
