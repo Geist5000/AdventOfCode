1657160d071e899a8c07c40e43846ec8862af4e7
Geist5000 <geist5000@gmail.com>
2021-11-01 09:32:52 +0100

    solved part 2 of 2018 day 1

--- 2018/1/second.py
+++ 2018/1/second.py
@@ -1,10 +1,18 @@
+import itertools
+
+
 lines = []
 
 
 with open("data.txt") as f:
     lines = list(map(lambda l: int(l.replace("\n","")),f.readlines()))
 
+resultSet = set([0])
 result = 0
-for l in lines:
+for l in itertools.cycle(lines):
     result += l
+    if result in resultSet:
+        break
+    else:
+        resultSet.add(result)
 print(result)
