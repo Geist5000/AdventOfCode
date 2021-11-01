793df8df4b0ccee15c164ca938af918e54c2b7c1
Geist5000 <geist5000@gmail.com>
2021-11-01 10:05:59 +0100

    Finished part 1 of 2018 day 2

--- 2018/2/first.py
+++ 2018/2/first.py
@@ -7,13 +7,36 @@ def loadData(fileName:str, mapFunc: LambdaType = lambda x: x):
         return list(map(lambda l: mapFunc(l.replace("\n","")),f.readlines()))
 
 def main(fileName:str):
-    lines = loadData(fileName)
+    lines = loadData(fileName, countLetters)
+    count2 = 0
+    count3 = 0
     for l in lines:
-        print(l)
+        c2 = False
+        c3 = False
+        for c,count in l.items():
+            if(count == 2):
+                c2 = True
+            elif count == 3:
+                c3 = True
+        if c2:
+            count2+= 1
+        if c3:
+            count3+= 1
+    print(count2*count3)
+        
+
+def countLetters(id:str):
+    count = {}
+    for c in id:
+        if c not in count:
+            count[c] = 1
+        else:
+            count[c] += 1
+    return count
 
 
 if __name__ == "__main__":
-    test = True
+    test = False
     if(test):
         fileName = "testInput.txt"
     else:
