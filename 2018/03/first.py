1a7a3482d0fd97413eeb6374c733994eeaca0fba
Geist5000 <geist5000@gmail.com>
2021-11-07 14:09:30 +0100

    finished part one of day 03 2018

--- 2018/03/first.py
+++ 2018/03/first.py
@@ -1,21 +1,59 @@
 from types import LambdaType
+import re
+from dataclasses import dataclass
 
 
-def loadData(fileName:str, mapFunc: LambdaType = lambda x: x):
-    
+@dataclass
+class Claim:
+    id: int
+    x: int
+    y: int
+    width: int
+    height: int
+
+    def forEach(self):
+        for x in range(self.x, self.x+self.width):
+            for y in range(self.y, self.y+self.height):
+                yield x, y
+
+
+def loadData(fileName: str, mapFunc: LambdaType = lambda x: x):
+
     with open(fileName) as f:
-        return list(map(lambda l: mapFunc(l.replace("\n","")),f.readlines()))
+        return list(map(lambda l: mapFunc(l.replace("\n", "")), f.readlines()))
 
-def main(fileName:str):
+
+def main(fileName: str):
+    regex = re.compile(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)')
     lines = loadData(fileName)
-    for index,l in enumerate(lines):
-        print(l)
+    claims = list()
+    xDict = dict()
+    overlappCount = 0
+    for index, l in enumerate(lines):
+        claim = parseClaim(regex, l)
+        for x, y in claim.forEach():
+            if x not in xDict.keys():
+                xDict[x] = dict()
+            yDict:dict = xDict[x]
+            if y in yDict:
+                if yDict[y] == 1:
+                    overlappCount += 1
+                yDict[y]+=1
+                claim.overlappCount += 1
+            else:
+                yDict[y] = 1
+    return overlappCount
+
+
+def parseClaim(regex: re.Pattern, claim: str):
+    match = regex.fullmatch(claim)
+    return Claim(int(match[1]), int(match[2]), int(match[3]), int(match[4]), int(match[5]))
 
 
 if __name__ == "__main__":
-    test = True
+    test = False
     if(test):
         fileName = "testInput.txt"
     else:
         fileName = "data.txt"
-    main(fileName)
+    print(main(fileName))
