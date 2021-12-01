6d1c96f343e1d3714ff928f40ba513f10ebbeeda
Geist5000 <geist5000@gmail.com>
2021-12-01 08:25:57 +0100

    finished day 1 part 1 of 2021

---
+++ 2021/01/first.py
@@ -0,0 +1,35 @@
+from types import LambdaType
+
+
+def loadData(fileName:str, mapFunc: LambdaType = lambda x: x):
+    
+    with open(fileName) as f:
+        return list(map(lambda l: mapFunc(l.replace("\n","")),f.readlines()))
+
+def main(fileName:str):
+    lines = loadData(fileName,int)
+
+    last = lines[0]
+    inc_count = 0
+    dec_count = 0
+    same_count = 0
+    for index,n in enumerate(lines[1:]):
+        if last < n:
+            inc_count += 1
+        elif last > n:
+            dec_count += 1
+        else:
+            same_count += 1
+        last = n
+    
+    print(f"Increased Count: {inc_count}")
+
+
+
+if __name__ == "__main__":
+    test = False
+    if(test):
+        fileName = "testInput.txt"
+    else:
+        fileName = "data.txt"
+    main(fileName)
